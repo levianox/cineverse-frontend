612bcdce30ca5e561229e583f64124bd6ec6327c
Jéssica Araujo <jessicaraujo37@gmail.com>
2021-01-16 16:45:09 +0000

    Adicionados formulários para editar filmes

--- src/app/main/movie-form/movie-form.component.ts
+++ src/app/main/movie-form/movie-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from 'src/app/models/Movie';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-movie-form',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieFormComponent implements OnInit {
 
+  @Input() movie: any;
+
+
+  movieForm = new FormGroup({
+      title: new FormControl(''),
+      description: new FormControl('')
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  saveForm(){
+    console.log(this.movieForm.value);
   }
 
 }
